11bd3fc9396d6344034221a18ddb28e0ba27edb5
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2022-01-15 17:23:09 +0530

    Create update_hook_object_object.js

---
+++ update_hook_object_object.js
@@ -0,0 +1,8 @@
+ setExampleState({...exampleState,  masterField2: {
+        fieldOne: "a",
+        fieldTwo: {
+           fieldTwoOne: "b",
+           fieldTwoTwo: "c"
+           }
+        },
+   })
