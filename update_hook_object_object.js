c0b461b758b0335379e0d491db431b16db4336ed
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2022-01-15 17:27:41 +0530

    Update update_hook_object_object.js

--- update_hook_object_object.js
+++ update_hook_object_object.js
@@ -6,3 +6,43 @@
            }
         },
    })
+
+
+const [infoData, setInfoData] = useState({
+    major: {
+      name: "John Doe",
+      age: "24",
+      sex: "M",
+    },
+
+    minor:{
+      id: 4,
+      collegeRegion: "south",
+
+    }
+
+  });
+Updating a specific record will require making a recall to the previous State prevState
+
+Here:
+
+setInfoData((prevState) => ({
+      ...prevState,
+      major: {
+        ...prevState.major,
+        name: "Tan Long",
+      }
+    }));
+perhaps
+
+setInfoData((prevState) => ({
+      ...prevState,
+      major: {
+        ...prevState.major,
+        name: "Tan Long",
+      },
+      minor: {
+        ...prevState.minor,
+        collegeRegion: "northEast"
+
+    }));
